966603f748198c29cae19e1e6dd20c2f67990c37
q-u-n <159257895+q-u-n@users.noreply.github.com>
2024-04-01 05:24:18 +0000

    chore: immer exp

---
+++ examples/zustand/immerjs/src/App.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { produce } from "immer";
+
+const useStore = create((set) => ({
+  deep: {
+    nested: {
+      obj: {
+        count: 1,
+      },
+    },
+  },
+  increment: () =>
+    set((state) =>
+      produce(state, (draft) => {
+        draft.deep.nested.obj.count++;
+      })
+    ),
+}));
+
+export default function App() {
+  const {
+    deep: {
+      nested: {
+        obj: { count },
+      },
+    },
+    increment,
+  } = useStore();
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={increment}>+1</button>
+    </div>
+  );
+}

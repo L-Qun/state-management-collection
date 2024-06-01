13d5900d17f7317f39a3b28282f25bafc3a259e4
q-u-n <159257895+q-u-n@users.noreply.github.com>
2024-06-01 17:00:57 +0000

    feat: simulate heavy computation

--- examples/react-query/compiler/app/page.tsx
+++ examples/react-query/compiler/app/page.tsx
@@ -1,20 +1,31 @@
-"use client"
+'use client'
 
-import { useState } from "react"
+import { useState } from 'react'
 
 const Component = () => {
   console.log('re-render')
   return <div>Component</div>
 }
 
+// 模拟繁重的 JS 逻辑
+function simulateHeavyComputation() {
+  console.log('simulateHeavyComputation')
+  const start = performance.now()
+  while (performance.now() - start < 2) {}
+  return 'mock res'
+}
+
 export default function Home() {
   const [count, setCount] = useState(0)
+
+  const res = simulateHeavyComputation() // 这里要真正被组件中用到
 
   return (
     <>
       <span>count: {count} </span>
-      <button onClick={() => setCount(count => count + 1)}>+1</button>
+      <button onClick={() => setCount((count) => count + 1)}>+1</button>
       <Component />
+      {res}
     </>
   )
 }

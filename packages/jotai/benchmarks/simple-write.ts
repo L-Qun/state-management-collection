d3cecd6a031924e2ec7cb52f56681b6b30aa3e14
qun <778157949@qq.com>
2024-07-13 22:59:15 +0800

    feat: benchmarks

--- packages/jotai/benchmarks/simple-write.ts
+++ packages/jotai/benchmarks/simple-write.ts
@@ -1,50 +1,55 @@
 #!/usr/bin/env npx tsx
 
-import { add, complete, cycle, save, suite } from "benny";
-import { PrimitiveAtom, atom } from "../src/atom";
+import { add, complete, cycle, save, suite } from 'benny'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-import { createStore } from "../src/store";
+import { PrimitiveAtom, atom } from '../src/atom'
+import { createStore } from '../src/store'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const createStateWithAtoms = (n: number) => {
-  let targetAtom: PrimitiveAtom<number> | undefined;
-  const store = createStore();
+  let targetAtom: PrimitiveAtom<number> | undefined
+  const store = createStore()
   for (let i = 0; i < n; ++i) {
-    const a = atom(i);
+    const a = atom(i)
     if (!targetAtom) {
-      targetAtom = a;
+      targetAtom = a
     }
-    store.set(a, i);
+    store.set(a, i)
   }
   if (!targetAtom) {
-    throw new Error();
+    throw new Error()
   }
-  return [store, targetAtom] as const;
-};
+  return [store, targetAtom] as const
+}
 
 const suiteCases = [2, 3, 4, 5, 6].map((n) =>
   add(`atoms=${10 ** n}`, () => {
-    const [store, targetAtom] = createStateWithAtoms(10 ** n);
-    return () => store.set(targetAtom, (c) => c + 1);
-  })
-);
+    const [store, targetAtom] = createStateWithAtoms(10 ** n)
+    return () => store.set(targetAtom, (c) => c + 1)
+  }),
+)
 
 const main = async () => {
   await suite(
-    "simple-write",
+    'simple-write',
     ...suiteCases,
     cycle(),
     complete(),
     save({
       folder: __dirname,
-      file: "simple-write",
-      format: "json",
+      file: 'simple-write',
+      format: 'json',
     }),
     save({
       folder: __dirname,
-      file: "simple-write",
-      format: "chart.html",
-    })
-  );
-};
+      file: 'simple-write',
+      format: 'chart.html',
+    }),
+  )
+}
 
-main();
+main()
